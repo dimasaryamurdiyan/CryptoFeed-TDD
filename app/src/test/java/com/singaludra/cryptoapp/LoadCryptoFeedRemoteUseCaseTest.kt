8e4356370962399da968369a34baaf3c8994773d
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-10-14 15:40:22 +0700

    improve function name

--- app/src/test/java/com/singaludra/cryptoapp/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/singaludra/cryptoapp/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -9,7 +9,7 @@ import org.junit.Test
 
 class LoadCryptoFeedRemoteUseCaseTest {
     @Test
-    fun testInitDoesNotLoad(){
+    fun testInitDoesNotRequestData(){
         val (_, client) = makeSut()
         LoadCryptoFeedRemoteUseCase(httpClient = client)
 
