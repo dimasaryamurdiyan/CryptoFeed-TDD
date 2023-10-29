feb195386396f3f9cef22b00c40bce2d20f0db70
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-10-29 19:18:19 +0700

    add connectivity exception when load http client

--- app/src/main/java/com/singaludra/cryptoapp/api/LoadCryptoFeedRemoteUseCase.kt
+++ app/src/main/java/com/singaludra/cryptoapp/api/LoadCryptoFeedRemoteUseCase.kt
@@ -1,13 +1,22 @@
 package com.singaludra.cryptoapp.api
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+
+
 interface HttpClient {
-    fun get()
+    fun get() : Flow<Exception>
 }
 
 class LoadCryptoFeedRemoteUseCase constructor(
     private val httpClient: HttpClient
 ) {
-    fun load(){
-        httpClient.get()
+    fun load(): Flow<Exception> = flow {
+        httpClient.get().collect{ error ->
+            emit(Connectivity())
+        }
     }
 }
+
+class Connectivity: Exception()
